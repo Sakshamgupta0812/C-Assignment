e765dfc493ba9633cbddf69f5149f836b446f676
Sakshamgupta0812 <101275740+Sakshamgupta0812@users.noreply.github.com>
2025-09-24 11:46:08 +0530

    1 assignment

--- Assignment8.c
+++ Assignment8.c
@@ -3,11 +3,14 @@
 int main()
 {
     int a,b,num=1;
-    printf("Enter two numbers: ");
-    scanf("%d %d",&a,&b);
+    printf("Enter the numbers: ");
+    scanf("%d",&a);
+    printf("Enter the power: ");
+    scanf("%d",&b);
     for(int i=0;i<b;i++)
     {
         num *= a;
     }
     printf("%d^%d = %d\n",a,b,num);
+    return 0;
 }
